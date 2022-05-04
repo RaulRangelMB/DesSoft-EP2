4459e09b6b24325ad61a75bf531b6fca4b6616a8
raulrangel <pidapunia90@gmail.com>
2022-05-04 08:29:34 -0300

    Sistema de vitoria e derrota

--- jogo.py
+++ jogo.py
@@ -1,24 +1,38 @@
 import scriptsbase as sb
 from database import DADOS, EARTH_RADIUS
 
-tentativas = 20
 raio = EARTH_RADIUS
 paises = sb.normaliza(DADOS)
-pais_sorte = sb.sorteia_pais(paises)
 tent_paises = {}
+jogar = 's'
 
-while tentativas > 0:
-    guess = input("adivinhe um país: ").lower()
-    if guess == 'dicas':
-        print('dicas')
+while jogar == 's':
+    tentativas = 20
+    pais_sorte = sb.sorteia_pais(paises)
+    while tentativas > 0:
+        guess = input("Adivinhe um país: ").lower()
+        if guess == pais_sorte:
+            print('Você venceu!!!')
+            break
+
+        elif guess == 'desisto':
+            certeza = input("Tem certeza que quer desistir dessa rodada? [s/n] ").lower()
+            if certeza == 's':
+                print('Você é fraco. O país era {0}'.format(pais_sorte))
+                tentativas = 0
+                break
         
-    elif guess not in paises:
-        print("País desconhecido, tente outro...")
-    
-    else:
-        print("Sim")
-        tentativas -= 1
-        tent_paises[guess] = sb.haversine(raio, paises[guess]['geo']['latitude'], paises[guess]['geo']['longitude'], paises[pais_sorte]['geo']['latitude'], paises[pais_sorte]['geo']['longitude'])
-        print(pais_sorte, tent_paises)
-        print('Você tem : {} tentativas sobrando!'.format(tentativas))
-
+        elif guess == 'dicas':
+            print('dicas')
+            
+        elif guess not in paises:
+            print("País desconhecido, tente outro...")
+        
+        else:
+            print("Sim")
+            tentativas -= 1
+            tent_paises[guess] = sb.haversine(raio, paises[guess]['geo']['latitude'], paises[guess]['geo']['longitude'], paises[pais_sorte]['geo']['latitude'], paises[pais_sorte]['geo']['longitude'])
+            print(pais_sorte, tent_paises)
+            print('Você tem : {} tentativas sobrando!'.format(tentativas))
+    jogar = input('Quer jogar novamente? [s/n] ').lower()
+print('falou!')
